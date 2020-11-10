12beb1ae5e9b17bf818ad5ec31bc9c9a86d8850d
Renzo Marcos <50187055+RenzoMarcos@users.noreply.github.com>
2020-11-10 05:25:19 -0500

    Registro_wBug

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Renzo\OneDrive\Documentos\GitHub\ToDo-List---ToDo-Friends\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
