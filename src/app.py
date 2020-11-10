cb7ed7163106ee21c6cb957a0991351af9213204
Brayan.oroncuy <50337678+brayanoroncuy123@users.noreply.github.com>
2020-11-09 19:35:04 -0500

    Update app

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ mysql=MySQL()
 app.config["MYSQL_DATABASE_USER"]='root'
 app.config["MYSQL_DATABASE_PASSWORD"]=''
 app.config["MYSQL_DATABASE_DB"]='bbdd'
-app.config["MYSQL_DATABASE_HOST"]='localhost'
+#app.config["MYSQL_DATABASE_HOST"]='localhost'
 mysql.init_app(app)
 
 @app.route("/login", methods=['GET', 'POST'])
