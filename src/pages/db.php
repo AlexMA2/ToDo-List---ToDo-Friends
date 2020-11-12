6bc0812e2f298498dd8df1854454c1055487ccdf
Brayan.oroncuy <50337678+brayanoroncuy123@users.noreply.github.com>
2020-11-12 18:04:09 -0500

    login error

--- src/pages/db.php
+++ src/pages/db.php
@@ -1,4 +1,4 @@
 <?php
-    $conexion=new mysqli("localhost", "root", "", "bbdd");
-    //$conexion=mysqli_connect("localhost", "root", "", "bbdd");
+    //$conexion=new mysqli("localhost", "root", "", "bbdd");
+    $conexion=mysqli_connect("localhost", "root", "", "bbdd");
 ?>
