684d32e171daac11e8dfc389f2d07bfbe17d0211
Renzo Marcos <50187055+RenzoMarcos@users.noreply.github.com>
2020-11-13 00:22:33 -0500

    .

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Renzo\OneDrive\Documentos\GitHub\ToDo-List---ToDo-Friends\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
