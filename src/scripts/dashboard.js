9032630dd8e495386a503bbae297975fc9041a04
Alex <58560420+AlexMA2@users.noreply.github.com>
2020-11-17 12:21:04 -0500

    Funcion de Editar Tarea

--- src/scripts/dashboard.js
+++ src/scripts/dashboard.js
@@ -1,16 +1,32 @@
 $(function () {
-	
-	var titulo_tarea;
-	$(".lista-tareas").on("click", function (event) {
+
+	var cambios = new Array(3);
+	$(".lista-tareas").on("click", ".item-tarea", function (event) {
 		event.preventDefault();
 		let x = event.target;
-		if (x.nodeName === 'I' || x.nodeName === 'A') {
-			$("#overlay").addClass('active');
-			$("#popup").addClass('active');
+
+		if (x.nodeName === 'A' || x.nodeName === 'I') {
+			if ($(x).hasClass("btn-editar")) {
+
+				$("#overlay").addClass('active');
+				$("#popup").addClass('active');
+
+				$("#inEditTitulo").val($(this).children(".titulo-tarea").text());
+				$("#inEditDesc").val($(this).children(".desc-tarea").text());
+				$("#inEditFecha").val($(this).children(".fecha-tarea").text());
+
+				cambios[0] = $(this).children(".titulo-tarea");
+				cambios[1] = $(this).children(".desc-tarea");
+				cambios[2] = $(this).children(".fecha-tarea");
+
+			}
+			else if ($(x).hasClass("btn-eliminar")) {
+				//Codigo para eliminar la tarea
+			}
 		}
-		titulo_tarea = $(this).find('td:first').text();
-		
+
 	});
+
 
 	$(".btn-cerrar-popup").on("click", function (event) {
 		event.preventDefault();
@@ -19,23 +35,23 @@ $(function () {
 			$("#overlay").removeClass('active');
 			$("#popup").removeClass('active');
 		}
-	});
-	
-	var acciones = '';
-	acciones += '<td>';
-	acciones += '<abbr title="Modificar Tarea">';
-	acciones += '<a href="#" class="btn btn-warning btn-editar"><i class="fas fa-pen"></i></a>';
-	acciones += '</abbr>';
-	acciones += ' <abbr title="Eliminar Tarea">';
-	acciones += '<a href="#" class="btn btn-danger btn-eliminar"><i class="fas fa-trash"></i></a>'
-	acciones += '</abbr>';
-	acciones += '</td>';
+	});	
 
 	crearTarea = (titulo, desc, fecha) => {
-		$(".lista-tareas").append("<tr></tr>");
-		$(".lista-tareas tr:last").append("<td>" + titulo + "</td>");
-		$(".lista-tareas tr:last").append("<td>" + desc + "</td>");
-		$(".lista-tareas tr:last").append("<td>" + fecha + "</td>");
+		let acciones = '';
+		acciones += '<td>';
+		acciones += '<abbr title="Modificar Tarea">';
+		acciones += '<a href="#" class="btn btn-warning btn-editar"><i class="fas fa-pen btn-editar"></i></a>';
+		acciones += '</abbr>';
+		acciones += ' <abbr title="Eliminar Tarea">';
+		acciones += '<a href="#" class="btn btn-danger btn-eliminar"><i class="fas fa-trash btn-eliminar"></i></a>'
+		acciones += '</abbr>';
+		acciones += '</td>';
+
+		$(".lista-tareas").append("<tr class='item-tarea'></tr>");
+		$(".lista-tareas tr:last").append("<td class='titulo-tarea'>" + titulo + "</td>");
+		$(".lista-tareas tr:last").append("<td class='desc-tarea'>" + desc + "</td>");
+		$(".lista-tareas tr:last").append("<td class='fecha-tarea'>" + fecha + "</td>");
 		$(".lista-tareas tr:last").append(acciones);
 	};
 
@@ -57,19 +73,36 @@ $(function () {
 					"Sin fecha límite"
 				);
 			}
-			$("#inTitulo").val("");
-			$("#inDesc").val("");
-			$("#inFecha").val("");
+
 		}
+
+		$("#inTitulo").val("");
+		$("#inDesc").val("");
+		$("#inFecha").val("");
+
 	});
 
-	editarTarea = () => {
-		console.log(" TAREAS ");
-		$(".lista-tareas").each(index => {
-			console.log(index + ": " + $(this).text());
-		});
-	}
-	
-	editarTarea();
+	$("#formEditarTarea").on("submit", function (event) {
+		event.preventDefault();
+		if ($("#inEditTitulo").val() !== "" && $("#inEditDesc").val() !== "") {
 
+			cambios[0].text($("#inEditTitulo").val());
+			cambios[1].text($("#inEditDesc").val());
+
+			if ($("#inEditFecha").val() !== "") {
+				cambios[2].text($("#inEditFecha").val());
+			} else {
+				cambios[2].text("Sin fecha límite");
+			}
+
+			$("#inEditTitulo").val("");
+			$("#inEditDesc").val("");
+			$("#inEditFecha").val("");
+		}
+
+		$("#overlay").removeClass('active');
+		$("#popup").removeClass('active');
+
+	});
+	
 });
