3ccfc3394709fe593261a99055370e3a711e4beb
Gianela_Mallqui <gianela.mallqui@unmsm.edu.pe>
2020-11-22 18:36:59 -0500

    Index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
             setcookie(session_name(), '', time() - 42000,
                     $params["path"], $params["domain"],
                     $params["secure"], $params["httponly"]
-                    );
+                );
         }
        
         session_destroy();
