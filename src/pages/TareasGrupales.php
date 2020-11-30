c9fc196a100b8f593f93c112a3a215e84b8cdd79
Brayan.oroncuy <50337678+brayanoroncuy123@users.noreply.github.com>
2020-11-29 19:15:25 -0500

    boton inicio

--- src/pages/TareasGrupales.php
+++ src/pages/TareasGrupales.php
@@ -163,7 +163,7 @@
                         <div class="col-sm-6">
 
                             <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#"> Inicio </a></li>
+                                <li class="breadcrumb-item"><a href="NetWork.php"> Inicio </a></li>
                                 <li class="breadcrumb-item active"> Tareas </li>
                             </ol>
                         </div>
