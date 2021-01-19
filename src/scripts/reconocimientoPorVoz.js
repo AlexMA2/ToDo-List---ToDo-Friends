0ac0b6d1960bf3225bb098dcd49a240c975f31b5
Néstor <66892045+NestorSoto@users.noreply.github.com>
2021-01-19 18:05:55 -0500

    recinocimiento de voz prueba 1

---
+++ src/scripts/reconocimientoPorVoz.js
@@ -0,0 +1,31 @@
+const $titulo = document.querySelector('#titleButton')
+const $descripcion = document.querySelector('#descripButton')
+var text=''
+//const $text=document.querySelector('inTitulo')
+const $text = document.querySelector('#inTitulo')
+const iniciar = (e) => {
+    
+	for (i = e.resultIndex; i < e.results.length; i++) {
+		$text.value = e.results[i][0].transcript
+	}
+}
+if(!('webkitSpeechRecognition' in window)){
+    alert('No se puede usar la api para reconocimiento de voz');
+}else{
+    alert('si puede usar la api');
+    rec = new webkitSpeechRecognition()
+	rec.lang = 'es-PE'
+	rec.continuous = true
+	rec.interim = true
+	rec.addEventListener('result', iniciar)
+	rec.start()
+}
+
+const Ttitulo=()=>{
+    
+}
+const tdescripcion=()=>{
+
+}
+$titulo.addEventListener('click', Ttitulo)
+$descripcion.addEventListener('click',tdescripcion)
